8c8a21908318e01df8739f4e936ff25526e59c80
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-11-07 23:15:00 -0500

    update docs. use pkgdown to generate documentation

---
+++ docs/index_files/phylocanvas-binding-0.0.0.9000/phylocanvas.js
@@ -0,0 +1,56 @@
+var gparams, tre;
+
+HTMLWidgets.widget({
+
+  name: 'phylocanvas',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+        //pass params globally
+
+        var tree = Phylocanvas.default.createTree(el);
+
+        //set global variables
+        tree.setTreeType(x.treetype);
+        tree.setNodeSize(x.nodesize);
+        tree.setTextSize(x.textsize);
+        tree.lineWidth = x.linewidth;
+        tree.showLabels = x.showlabels;
+        tree.alignLabels = x.alignlabels;
+
+        gparams = x;
+        tre = tree;
+
+        // loading will draw the tree
+        tree.load(x.tree);
+
+        // apply node styles after the tree is loaded
+        if (x.nodestyles) {
+          Object.keys(x.nodestyles).forEach(function(key){
+          var leaf = tree.findLeaves(key)[0];
+          leaf.setDisplay( x.nodestyles[key] );
+          leaf.highlighted = x.nodestyles[key].highlighted;
+          });
+        }
+
+        // activate all styles
+        tree.draw();
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
