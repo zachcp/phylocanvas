43308f033f88301f7420546462dea3cddc74f970
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-11-08 10:35:37 -0500

    add more docs for style_node function

--- R/style_node.R
+++ R/style_node.R
@@ -2,16 +2,26 @@
 #'
 #' @param phycnv Required.
 #' @param nodeid Required.
-#'
+#' @param highlighted Optional. Default \code{FALSE}.  Whether to highlight the node.
+#' @param color Optional. Color of the node/branch
+#' @param shape Optional. Default \code{'circle'}. Can be either 'circle', 'square' or 'traingle'.
+#' @param nodesizeratio Optional. Default \code{1}. Scale ratio of the nodesize to the base node size.
+#' @param strokecolor Optional. Default \code{"black"}. Colors are strings in the format of names, rgb strings or hex values.
+#' @param fillcolor Optional. Default \code{"black"}. Scale ratio of the node.
+#' @param linewidth Optional. Default \code{1}.
+#' @param labelcolor Optional. Default \code{"black"}. Label color. Colors are strings in the format of names, rgb strings or hex values.
+#' @param labeltextsize Optional.Default \code{20}. Label size.
+#' @param linefont Optional. Default \code{"Arial"}. Label font.
+#' @param lineformat Optional. Default \code{"bold"}. Label format.
 #' @export
 style_node <- function(phycnv, nodeid,
                            highlighted = FALSE,
                            color = "black",
-                           shape  = 'circle', #// or square, triangle, star
-                           nodesizeratio = 3, #// ratio of the base node size
-                           strokecolor= "black", # '#0000ff',
+                           shape  = 'circle',
+                           nodesizeratio = 1,
+                           strokecolor= "black",
                            fillcolor ='black',
-                           linewidth = 2,
+                           linewidth = 1,
                            labelcolour='black',
                            labeltextsize=20, #// points
                            labelfont ='Arial',
