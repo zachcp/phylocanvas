ec9ff75ec49f3aa2536e1d5b2fb1437c48e965f2
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-12-17 16:46:45 -0500

    add a descentdant nodes function

--- R/utilities.R
+++ R/utilities.R
@@ -9,15 +9,35 @@ getNodeName <- function(phy, id) {
   df2$label
 }
 
-#' Derived from Laim Revel's blogpost:
+#' Derived from Liam Revel's blogpost:
 #' http://blog.phytools.org/2012/01/function-to-get-descendant-node-numbers.html
+#'
 #' @export
-getDescendants<-function(tree,node,curr=NULL){
+#'
+getDescendants <- function(tree, node, curr=NULL, onlytips=TRUE) {
   if(is.null(curr)) curr<-vector()
-  daughters<-tree$edge[which(tree$edge[,1]==node),2]
-  curr<-c(curr,daughters)
-  w<-which(daughters>=length(tree$tip))
-  if(length(w)>0) for(i in 1:length(w))
-    curr<-getDescendants(tree,daughters[w[i]],curr)
+  daughters <- tree$edge[which(tree$edge[,1]==node),2]
+  curr      <- c(curr,daughters)
+  w         <- which(daughters>=length(tree$tip))
+
+  if(length(w) > 0) {
+    for(i in 1:length(w)) {
+      curr <- getDescendants(tree,daughters[w[i]],curr)
+    }
+  }
+
   return(curr)
+}
+
+#' get descendent tips
+#'
+#' return a character vector of descendant node names given an ancestral node.
+#'
+#' @export
+#'
+getDescendantNames <- function(tree, node) {
+  descendants     <- getDescendants(tree, node)
+  descendantnames <- as.character(lapply(descendants, function(n) {getNodeName(phy = tree, id=n)}))
+  descendantnames <- descendantnames[descendantnames %in% tree$tip.label]
+  descendantnames
 }
