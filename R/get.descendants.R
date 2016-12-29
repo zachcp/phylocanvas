9530f0a8b010879b1a59b0ba881d4e3ade7ff59c
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-12-29 17:09:58 -0500

    add code to use phylobase for descendant/selection code

---
+++ R/get.descendants.R
@@ -0,0 +1,26 @@
+#' get descendants
+#'
+#' get the names of the tips of descendantds given a tree
+#' and the name of an internal node.
+#'
+#' @param tree Required. A \code{\link[phlobase] phloy4} object.
+#' @param nodename Required. A character corresponding to an internal node.
+#'
+#' @return a character vector of descendant names
+#'
+#' @importFrom phylobase tipLabels
+#' @importFrom phylobase subset
+#' @export
+#'
+#' @examples
+#' birdfile <- system.file("treedata/birdfamilies.tree", package="phylocanvas")
+#' tree <- load.tree(birdfile)
+#' node <- phylobase::MRCA(tree, c("Cerylidae", "Upupidae"))
+#' get.descendants(tree, node)
+#'
+get.descendants <- function(tree, nodename) {
+  if (!inherits(tree, "phylo4")) stop("get.Descendants requires a phylo4 object")
+
+  subtree <- subset(tree, node.subtree=nodename)
+  tips    <- as.character(tipLabels(subtree))
+}
